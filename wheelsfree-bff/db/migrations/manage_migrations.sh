48453ce4e1b86875010a83e5ba2db321a8232d9e
Larissa Marcich <lmarcich@thoughtworks.com>
2019-11-21 11:20:59 +0800

    [lari] hacky initial pass at getting migrations working

---
+++ wheelsfree-bff/db/migrations/manage_migrations.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ENV=$1
+if [ "$ENV" = "" ]; then
+    ENV="development"
+fi
+
+sleep 6
+ATTEMPTS=0
+./mybatis/bin/migrate up
+while [[ $? -ne 0 && $ATTEMPTS -lt 3 ]]; do
+    sleep 6
+    ATTEMPTS=$[$ATTEMPTS+1]
+    ./mybatis/bin/migrate up --env=$ENV
+done
--- wheelsfree-bff/src/main/java/com/wheelsfree/controller/ItemController.java
+++ wheelsfree-bff/src/main/java/com/wheelsfree/controller/ItemController.java
@@ -23,14 +23,6 @@ public class ItemController {
 
     @GetMapping("/items")
     public ItemsResponse getAllAvailableItems() {
-        //return new ItemsResponse(itemService.getInStockItems());
-        List<Item> items = Arrays.asList(
-                new Item(1, "Fancy Drone", "Good base model drone.", BigDecimal.valueOf(100.25), 50L, "https://images.pexels.com/photos/336232/pexels-photo-336232.jpeg?w=750"),
-                new Item(2, "Super Fancy Drone", "The best drone money can buy.", BigDecimal.valueOf(229.99), 10L, "https://images.pexels.com/photos/442587/pexels-photo-442587.jpeg?w=750"),
-                new Item(3, "Fancier Drone", "Starting to become an advanced drone user.", BigDecimal.valueOf(159.85), 25L, "https://images.pexels.com/photos/2100075/pexels-photo-2100075.jpeg?w=750")
-
-        );
-
-        return new ItemsResponse(items);
+        return new ItemsResponse(itemService.getInStockItems());
     }
 }
